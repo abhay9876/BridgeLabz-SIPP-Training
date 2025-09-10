c9ae5e77734ce3bc650d11c5e6bcb6e803cf36db
abhay9876 <singhabhayrna987@gmail.com>
2025-09-10 08:45:08 +0530

    [Abhay Singh] Add . Complete Assignment

---
+++ LambdaExpressions/Hospital.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Hospital {
+    public static void main(String[] args) {
+        List<String> patientIds = Arrays.asList("P101", "P102", "P103", "P104");
+
+        
+        patientIds.forEach(System.out::println);
+    }
+}
